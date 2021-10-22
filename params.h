82e22747463b5a8a0cb6cf2b4b171c9205a4490c
Barbár <bbk.barbar@gmail.com>
2021-10-22 07:22:18 +0200

    v2.4 b18

---
+++ params.h
@@ -0,0 +1,32 @@
+
+#define TITLE          "IoT TH sensor"
+#define SOFTWARE_NAME  "IoT TH sensor"
+
+#define LOCATION_NAME  "Nappali"
+#define IOT_DEVICE_ID   1
+
+#define PHASE_CHECKER_IP  "192.168.1.170"
+
+
+// =============================
+// Pinout configuration
+// =============================
+ 
+#define DHT11_PIN                         14 // 14 means D5 in NodeMCU board -> Pin for temperature and humidity sensor
+
+#define SOFT_SERIAL_RX                     5 //  5 means D1 in NodeMCU board
+#define SOFT_SERIAL_TX                     4 //  4 means D2 in NodeMCU board
+
+
+// Server functions
+#define SERVER_PORT                       80
+
+// connect to WiFi
+#define WIFI_TRY_COUNT                    50
+#define WIFI_WAIT_IN_MS                 1000
+
+// delay for normal working
+#define DELAY_BETWEEN_ITERATIONS_IN_MS 30000
+
+// problem handling
+#define ERROR_COUNT_BEFORE_RESTART         3
